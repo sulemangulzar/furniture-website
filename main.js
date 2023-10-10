6e932f5c85046a23052aacbd9fb0b65b1d35099d
sulemangulzar <sulemangulzar07@gmail.com>
2023-10-10 22:25:00 +0500

    changed

---
+++ main.js
@@ -0,0 +1,18 @@
+const mobileNav = document.querySelector('.mnav');
+const closeBtn = document.querySelector('.mnav__close-btn');
+const closeBtnIcn = document.querySelector('.mvan__close-btn-icon');
+
+const navOpenedClass = 'left-0';
+const navClosedClass = '-left-[300px]';
+const arrowLeftClass = 'ri-arrow-left-s-line';
+const arrowRightClass = 'ri-arrow-right-s-line';
+
+closeBtn.addEventListener('click', () => {
+    if (mobileNav.classList.contains(navClosedClass)) {
+        mobileNav.classList.toggle(navOpenedClass);
+    }
+
+    closeBtnIcn.classList.toggle(arrowLeftClass);
+    closeBtnIcn.classList.toggle(arrowRightClass);
+}
+);
